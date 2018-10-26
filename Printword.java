4e3d68d686c99f88d37246ffa768aee6b00dd474
Madusudhan Reddy <ymadusudhanreddy@paypal.com>
2018-10-26 10:14:38 +0530

    Both Printword java and class files are added

---
+++ Printword.java
@@ -0,0 +1,18 @@
+package strings;
+
+class Stroy{
+	String s ="MadusudhanReddy";
+	
+public void method1(){
+	System.out.println(s+ " working in HCL");
+}
+}
+public class Printword {
+
+	public static void main(String[] args) {
+
+		Stroy story= new Stroy();
+		story.method1();
+		}
+
+}
